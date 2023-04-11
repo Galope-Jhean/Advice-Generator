5e658878c5ae6378e4f632d5f2479250f036aaf0
Jhean <drakekhendrick@gmail.com>
2023-04-12 00:33:55 +0800

    finished mobile state

---
+++ advice.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button");
+const adviceText = document.querySelector(".advice");
+const adviceID = document.querySelector(".header");
+const id = 0;
+
+
+//api fetch
+async function getAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+        adviceText.textContent = `“ ${data.slip.advice} ”`;
+        adviceID.textContent = `ADVICE #${data.slip.id}`;
+    });
+}
+
+window.onload = () => {
+    getAdvice();
+}
+
+button.addEventListener("click", () => {getAdvice()});
